f7c7342dbb0dee200dc056c23a899710ae4f0eb4
byj9402 <65005479+byj9402@users.noreply.github.com>
2022-04-20 17:54:08 +0900

    [Bronze II] Title: 팩토리얼, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/7489. 팩토리얼/팩토리얼.py
+++ 백준/Bronze/7489. 팩토리얼/팩토리얼.py
@@ -1,25 +1,16 @@
 import sys
+impot = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
-def fact(num) :
-    if num <= 1 :
-        return 1
-    return num * fact(num-1)
+def newfact(n) :
+    num = 1
+    for i in range(1, n+1) :
+        num *= i
+        while num % 10 == 0 : num = num // 10
+    return int(num % 10)
 
 t = int(input())
 
-for _ in range (0, t) :
+for _ in range (0,t) :
     N = int(input())
-    N_fact = fact(N)
-    N_str = str(N_fact)
-    N_len = len(N_str)
-
-    for i in range (0, N_len) :
-        if N_fact % 10 != 0 :
-            break
-        else :
-            N_fact = N_fact // 10
-
-    N_str = str(N_fact)
-
-    print(N_str[len(N_str)-1:])
+    print(newfact(N))
