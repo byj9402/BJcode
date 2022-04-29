0abcd3e460da3b79f9925532d14d5527aacffbea
BaeSH <65005479+byj9402@users.noreply.github.com>
2022-04-29 20:02:18 +0900

    [Silver IV] Title: 보물, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort(reverse = True)
+Sum = 0
+
+for i in range(N) :
+    Sum += A[i] * B[i]
+    
+print(Sum)
