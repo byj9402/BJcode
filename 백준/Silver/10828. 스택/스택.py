1cc98f4685fe3676e345ca6427880025013da5d9
BaeSH <65005479+byj9402@users.noreply.github.com>
2022-04-30 21:04:26 +0900

    [Silver IV] Title: 스택, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+
+for _ in range(N) :
+    command = input().split()
+
+    if command[0] == 'push' : stack.insert(0, command[1])
+    elif command[0] == 'size' : print(len(stack))
+    elif command[0] == 'pop' :
+        if len(stack) != 0 :
+            print(stack[0])
+            del stack[0]
+        else : print('-1')
+    elif command[0] == 'empty' :
+        if len(stack) != 0 : print('0')
+        else : print('1')
+    elif command[0] == 'top' :
+        if len(stack) != 0 : print(stack[0])
+        else : print('-1')
