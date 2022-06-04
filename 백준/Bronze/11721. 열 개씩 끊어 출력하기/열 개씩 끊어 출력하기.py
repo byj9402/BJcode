fc4c68b4a13c05fdd3c64636f7e52e8ffd3174ff
BaeSH <65005479+byj9402@users.noreply.github.com>
2022-06-04 23:09:30 +0900

    [Bronze II] Title: 열 개씩 끊어 출력하기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
+++ 백준/Bronze/11721. 열 개씩 끊어 출력하기/열 개씩 끊어 출력하기.py
@@ -2,11 +2,5 @@ import sys
 input = sys.stdin.readline
 
 line = input().strip()
-i = 0
-
-while (True) :
-    j = i + 10
-    if j > len(line) : j = len(line)
-    print(line[i:j])
-    i += 10
-    if j == len(line) : break
+for i in range(0, len(line), 10) :
+    print(line[i:i+10])
