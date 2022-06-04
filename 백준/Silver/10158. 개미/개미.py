173e3d5f65ecbb05113c6661d2499e3faa4e4c47
BaeSH <65005479+byj9402@users.noreply.github.com>
2022-06-04 09:47:28 +0900

    [Silver IV] Title: 개미, Time: 88 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10158. 개미/개미.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+w, h = map(int, input().split())
+p, q = map(int, input().split())
+t = int(input())
+
+if ((p + t) // w) % 2 == 0 : x = (p + t) % w
+else : x = w - (p + t) % w
+
+if ((q + t) // h) % 2 == 0 : y = (q + t) % h
+else : y = h - (q + t) % h
+
+print(x, y)
