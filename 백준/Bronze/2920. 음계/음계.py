3b984a9b0762dbf672782cfb2f82d93c43b1e9c5
BaeSeoHyeon <65005479+byj9402@users.noreply.github.com>
2024-04-03 14:58:08 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2920. 음계/음계.py
+++ 백준/Bronze/2920. 음계/음계.py
@@ -1,10 +1,6 @@
 import sys
-
-ascending = [i for i in range(1, 9)]
-descending = sorted(ascending, reverse = True)
-
 sound = list(map(int, sys.stdin.readline().split()))
 
-if ascending == sound : print('ascending')
-elif descending == sound : print('descending')
+if [1, 2, 3, 4, 5, 6, 7, 8] == sound : print('ascending')
+elif [8, 7, 6, 5, 4, 3, 2, 1] == sound : print('descending')
 else : print('mixed')
